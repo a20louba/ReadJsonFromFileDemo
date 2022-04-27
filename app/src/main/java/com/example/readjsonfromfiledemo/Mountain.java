9c6cf09dfc9310a7a31526d6e2ba25a3cabc7c64
a20louba <a20louba@student.his.se>
2022-04-27 15:34:51 +0200

    la till constructure och getters and setters

--- app/src/main/java/com/example/readjsonfromfiledemo/Mountain.java
+++ app/src/main/java/com/example/readjsonfromfiledemo/Mountain.java
@@ -6,5 +6,33 @@ public class Mountain {
     private String location;
     private int size;
 
+    public Mountain(String name, String location, int size) {
+        this.name = name;
+        this.location = location;
+        this.size = size;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
